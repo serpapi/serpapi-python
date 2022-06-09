391cdeb2fb35dd9f88f0a01aaafd7ffb776fbc26
Victor Benarbia <victor@serpapi.com>
2022-06-08 21:54:14 -0500

    improve python coding style

--- serpapi/serpapi.py
+++ serpapi/serpapi.py
@@ -4,132 +4,28 @@ import urllib3
 from .error import SerpApiException
 from .object_decoder import ObjectDecoder
 
-class Client(ObjectDecoder):
-    """
-    Client performend http query to serpApi.com
-     using urllib3 under the hood.
+class HttpClient:
+    """Simple HTTP client wrapper around urllib3"""
 
-    The HTTP connection be tuned to allow
-     - retries : attempt to reconnect if the connection fail by default: False
-     - timeout : connection timeout by default 60s
-    for more details:  https://urllib3.readthedocs.io/en/stable/user-guide.html
-    """
-
-    BACKEND = 'https://serpapi.com'
-    SUPPORTED_DECODER = ['json', 'html', 'object']
-
-    def __init__(self, parameter=None):
-        # urllib3 configuration
-        # 60s default
-        self.timeout = 60.0
-        # no HTTP retry
-        self.retries = False
+    def __init__(self, parameter: dict = {}):
         # initialize the http client
         self.http = urllib3.PoolManager()
 
-        # define default parameter
-        if parameter is None:
-            self.parameter = {}
+        # urllib3 configurations
+        # HTTP connect timeout 
+        if 'timeout' in parameter:
+            self.timeout = parameter['timeout']
         else:
-            # assign user parameter
-            self.parameter = parameter
-            # override default
-            if 'timeout' in parameter:
-                self.timeout = parameter['timeout']
-            if 'retries' in parameter:
-                self.retries = parameter['retries']
-
-    def search(self, parameter=None, decoder='json'):
-        """
-        make search then decode the output
-         decoder supported 'json', 'html', 'object'
-
-        Parameters
-        ----------
-        parameter : dict
-            search query
-        decoder : str
-            set decoder to convert the datastructure received from
-
-        Returns
-        -------
-        dict|str
-            search results returns as :
-             dict if decoder = 'json'
-             str if decoder = 'html'
-             object if decoder = 'object'
-        """
-        return self.start(path='/search', parameter=parameter, decoder=decoder)
-
-    def html(self, parameter=None):
-        """
-        html search
-
-        Parameters
-        ----------
-        parameter : dict
-            search query see: https://serpapi.com/search-api
-
-        Returns
-        -------
-        str
-        raw html search results directly from the search engine
-        """
-        return self.start('/search', parameter, 'html')
-
-    def location(self, parameter=None):
-        """
-        Get location using Location API
-
-        Parameters
-        ----------
-        parameter : dict
-            location query like: {q: "Austin", limit: 5}
-             see: https://serpapi.com/locations-api
-
-        Returns
-        -------
-        array
-        list of matching locations
-        """
-        return self.start('/locations.json', parameter, 'json')
-
-    def search_archive(self, search_id, decoder='json'):
-        """
-        Retrieve search results from the Search Archive API
-
-        Parameters:
-
-        """
-        path = "/searches/" + str(search_id) + "."
-        if decoder in self.SUPPORTED_DECODER:
-            if decoder == "object":
-                path += "json"
-            else:
-                path += decoder
-        else:
-            raise SerpApiException('Decoder must be json or html or object')
-        return self.start(path, {}, decoder)
-
-    def account(self, api_key=None):
-        """
-        Get account information using Account API
-
-        Parameters
-        ---
-        api_key: str
-          secret user key provided by serpapi.com
+            # 60s default
+            self.timeout = 60.0
         
-        Returns
-        ---
-        dict
-        user account information
-        """
-        if api_key is not None:
-            self.parameter['api_key'] = api_key
-        return self.start('/account', self.parameter, 'json')
+        # no HTTP retry
+        if 'retries' in parameter:
+            self.retries = parameter['retries']
+        else:
+            self.retries = False
 
-    def start(self, path, parameter=None, decoder='json'):
+    def start(self, path : str, parameter: dict = None, decoder : str = 'json'):
         """
         start HTTP request and decode response using urllib3
          the response is decoded using the selected decoder:
@@ -174,8 +70,8 @@ class Client(ObjectDecoder):
                                      retries=self.retries)
         # decode response
         return self.decode(response, decoder)
-    
-    def decode(self, response, decoder):
+
+    def decode(self, response: any, decoder: str):
         """Decode HTTP response using a given decoder"""
         # handle HTTP error
         if response.status != 200:
@@ -202,3 +98,116 @@ class Client(ObjectDecoder):
 
         raise SerpApiException("Invalid decoder: " +
                                decoder + ", available: json, html, object")
+
+class Client(ObjectDecoder, HttpClient):
+    """
+    Client performend http query to serpApi.com using urllib3 under the hood.
+
+    The HTTP connection be tuned to allow
+     - retries : attempt to reconnect if the connection fail by default: False
+     - timeout : connection timeout by default 60s
+    for more details:  https://urllib3.readthedocs.io/en/stable/user-guide.html
+
+    """
+
+    BACKEND = 'https://serpapi.com'
+    SUPPORTED_DECODER = ['json', 'html', 'object']
+
+    def __init__(self, parameter: dict = None):
+        # define default parameter
+        if parameter is None:
+            self.parameter = {}
+        else:
+            # assign user parameter
+            self.parameter = parameter
+        HttpClient.__init__(self, self.parameter)
+
+    def search(self, parameter: dict = None, decoder: str = 'json'):
+        """
+        make search then decode the output
+         decoder supported 'json', 'html', 'object'
+
+        Parameters
+        ----------
+        parameter : dict
+            search query
+        decoder : str
+            set decoder to convert the datastructure received from
+
+        Returns
+        -------
+        dict|str
+            search results returns as :
+             dict if decoder = 'json'
+             str if decoder = 'html'
+             object if decoder = 'object'
+        """
+        return self.start(path='/search', parameter=parameter, decoder=decoder)
+
+    def html(self, parameter: dict = None):
+        """
+        html search
+
+        Parameters
+        ----------
+        parameter : dict
+            search query see: https://serpapi.com/search-api
+
+        Returns
+        -------
+        str
+        raw html search results directly from the search engine
+        """
+        return self.start('/search', parameter, 'html')
+
+    def location(self, parameter: dict = None):
+        """
+        Get location using Location API
+
+        Parameters
+        ----------
+        parameter : dict
+            location query like: {q: "Austin", limit: 5}
+             see: https://serpapi.com/locations-api
+
+        Returns
+        -------
+        array
+        list of matching locations
+        """
+        return self.start('/locations.json', parameter, 'json')
+
+    def search_archive(self, search_id : str, decoder : str ='json'):
+        """
+        Retrieve search results from the Search Archive API
+
+        Parameters:
+
+        """
+        path = "/searches/" + str(search_id) + "."
+        if decoder in self.SUPPORTED_DECODER:
+            if decoder == "object":
+                path += "json"
+            else:
+                path += decoder
+        else:
+            raise SerpApiException('Decoder must be json or html or object')
+        return self.start(path, {}, decoder)
+
+    def account(self, api_key: str = None):
+        """
+        Get account information using Account API
+
+        Parameters
+        ---
+        api_key: str
+          secret user key provided by serpapi.com
+
+        Returns
+        ---
+        dict
+        user account information
+        """
+        if api_key is not None:
+            self.parameter['api_key'] = api_key
+        return self.start('/account', self.parameter, 'json')

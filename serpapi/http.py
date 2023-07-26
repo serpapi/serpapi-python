c65569e66348f9ebd0381c755e3baf14693d3ea2
Kenneth Reitz <me@kennethreitz.org>
2023-07-26 10:11:05 -0400

    docstring clarification

--- serpapi/http.py
+++ serpapi/http.py
@@ -8,7 +8,7 @@ from .__version__ import __version__
 
 
 class HTTPClient:
-    """A class that handles the HTTP requests to SerpApi.com."""
+    """This class handles outgoing HTTP requests to SerpApi.com."""
 
     BASE_DOMAIN = "https://serpapi.com"
     USER_AGENT = f"serpapi-python, v{__version__}"
